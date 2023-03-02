e2636cd8aaa9cf10033dd347a2d5d885691bf460
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-02 15:34:16 +0000

    Initial Commit

---
+++ UniversityList/UniversityListApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityListApp.swift
+//  UniversityList
+//
+//  Created by Don Bouncy on 02/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct UniversityListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
