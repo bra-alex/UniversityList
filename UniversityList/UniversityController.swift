c7840e92d8170b38d93d41708c694556ef920b28
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-02 16:07:51 +0000

    Add model and controller

---
+++ UniversityList/UniversityController.swift
@@ -0,0 +1,42 @@
+//
+//  UniversityController.swift
+//  UniversityList
+//
+//  Created by Don Bouncy on 02/03/2023.
+//
+
+import Foundation
+
+struct University: Codable{
+    let name: String
+    let state: String?
+    let alphaTwoCode: String
+    let domains: [String]
+    let webPages: [String]
+}
+
+class UniversityController: ObservableObject{
+    @Published var universities = [University]()
+    
+    func loadData() async {
+        guard let url = URL(string: "http://universities.hipolabs.com/search?country=United+States") else {
+            print("Unable to parse url")
+            return
+        }
+        
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.keyDecodingStrategy = .convertFromSnakeCase
+            
+            let decodedData = try decoder.decode([University].self, from: data)
+                DispatchQueue.main.async {
+                    self.universities = decodedData
+                }
+        } catch {
+            print(String(describing: error))
+        }
+        
+    }
+}
