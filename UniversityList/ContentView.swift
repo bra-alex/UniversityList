94046adb79e68cc9e4d12ece50a55a217963b583
Alexander Asiamah Boakye <aalexanderkwaku@yahoo.co.uk>
2023-03-02 17:05:09 +0000

    Show popup on tap

--- UniversityList/ContentView.swift
+++ UniversityList/ContentView.swift
@@ -9,18 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject private var uc = UniversityController()
+    @State private var showDetails = false
+    @State private var university: University?
     var body: some View {
         NavigationStack{
-            if !uc.universities.isEmpty{
-                List(uc.universities, id: \.name) { university in
-                    Text(university.name)
-                        .padding(.vertical, 7)
+            ZStack{
+                if !uc.universities.isEmpty{
+                    List(uc.universities, id: \.name) { university in
+                        Text(university.name)
+                            .padding(.vertical, 7)
+                            .onTapGesture {
+                                self.university = university
+                                showDetails = true
+                            }
+                    }
+                    .listStyle(.plain)
+                    .navigationTitle("Universities")
+                    .navigationBarTitleDisplayMode(.inline)
+                    
+                } else {
+                    ProgressView()
                 }
-                .listStyle(.plain)
-                .navigationTitle("American Universities")
-                .navigationBarTitleDisplayMode(.inline)
-            } else {
-                ProgressView()
+                
+                if showDetails{
+                    UniversityPopup(showDetails: $showDetails, university: university!)
+                }
             }
         }
         .task {
